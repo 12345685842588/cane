87a38a0a76a061eda3d23a61c2a846a6bb33b40a
Utente <Utente@DESKTOP-BAB4AAS>
2024-01-24 15:58:16 +0100

    Aggiungere i file di progetto.

---
+++ cane/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane c=new Cane();
+            c.Razza = "labrador";
+            c.Nome = "ringhio";
+            c.Proprietario.Nome = "simone";
+
+            Persona p=new Persona();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
